65c42f7ca1f07cef8e8d41994b63756505549379
Thomas McGrew <tjmcgrew@gmail.com>
2016-10-20 19:07:49 -0400

    Changed the setup file to accomodate tkinter

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import py2exe, sys, os
 sys.argv.append('py2exe')
  
 setup(
-    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    console = [{'script': "dwrandomizer.py"}],
+    options = {'py2exe': {'bundle_files': 2, 'compressed': True}},
+    console = [{'script': "randomizer_gui.py"}],
     zipfile = None,
 )
 

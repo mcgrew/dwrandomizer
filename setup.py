e1e1f735f8f27ce97b4c0d0182e9f93f49cf80c5
Thomas <tjmcgrew@gmail.com>
2016-11-20 19:28:33 -0500

    Created installer scripts
    Added an installer creation script for use with NSIS Quick Setup Script
      Generator
    Fixed setup.py to generate a windows app instead of a console app

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from distutils.core import setup
 import py2exe, sys, os
+from dwrandomizer import VERSION
  
 sys.argv.append('py2exe')
  
 setup(
+    name = 'dwrandomizer',
+    version = VERSION,
+    description = 'A randomizer for Dragon Warrior for NES',
+    url = 'http://github.com/mcgrew/dwrandomizer',
+    author = 'Thomas McGrew',
     options = {'py2exe': {'bundle_files': 2, 'compressed': True}},
-    console = [{'script': "randomizer_gui.py"}],
-    zipfile = None,
+    windows = [{'script': 'randomizer_gui.py', 'dest_base': 'dwrandomizer'}],
+    zipfile = None
 )
 
-#special thanks to Zurkei for this file :)
+# special thanks to Zurkei for figuring out how to make this work :)

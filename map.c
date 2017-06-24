4e1dd903c110137762df77d7d07cd7ac56b5306f
Thomas McGrew <tjmcgrew@gmail.com>
2017-06-24 03:12:40 -0400

    Implemented landmark placement

--- map.c
+++ map.c
@@ -8,6 +8,7 @@
 
 #define MAX_BLOB 2400
 #define MAP_ENCODED_SIZE 2534
+#define VIABLE_CONT 250 /* minimum size for a continent to use */
 
 static bool map_encode(dw_rom *rom)
 {
@@ -65,26 +66,87 @@ void map_decode(dw_rom *rom)
 {
 }
 
-static void map_find_land(dw_rom *rom, int *continents, int cont_count,
-        int *coords)
+static void map_find_land(dw_map *map, int one, int two, uint8_t *x, uint8_t *y)
 {
     int i;
-    uint8_t x, y, cont;
+    uint8_t cont;
 
     for (;;) {
-        coords[0] = mt_rand(0, 120);
-        coords[1] = mt_rand(0, 120);
-        cont = rom->map.walkable[x][y];
-        for (i=0; i < cont_count; i++) {
-            if (cont == continents[i]) {
-                return;
-            }
-        }
+        *x = mt_rand(0, 120);
+        *y = mt_rand(0, 120);
+        cont = map->walkable[*x][*y];
+        if (cont && (cont == one || cont == two))
+            return;
     }
 }
 
-static bool place_landmarks(dw_rom *rom)
+static void place(dw_map *map, dw_warp_index warp_idx, dw_tile tile, 
+        int lm_one, int lm_two)
 {
+    uint8_t x, y;
+    dw_warp *warp;
+    dw_tile old_tile;
+
+    warp = &map->warps_from[warp_idx];
+
+    do {
+        map_find_land(map, lm_one, lm_two, &x, &y);
+        old_tile = map->tiles[x][y];
+    } while (old_tile == TILE_CASTLE || old_tile == TILE_TOWN ||
+             old_tile == TILE_CAVE);
+
+    warp->map = 1;
+    warp->x = x;
+    warp->y = y;
+    map->tiles[x][y] = tile;
+//    printf("Placed %d at %d,%d\n", (int)warp_idx, x, y);
+}
+
+static bool place_landmarks(dw_map *map, int *cont_sizes, int cont_count)
+{
+    int i, largest = 0, next = 0;
+    dw_warp *warp;
+    dw_warp_index caves[6] = {
+            WARP_NORTHERN_SHRINE,
+            WARP_MOUNTAIN_CAVE,
+            WARP_SOUTHERN_SHRINE,
+            WARP_ERDRICKS_CAVE,
+            WARP_TANTEGEL_BASEMENT,
+            WARP_GARINS_GRAVE
+    };
+
+    /* find the 2 largest land masses */
+    for(i=0; i < cont_count; i++) {
+        if (cont_sizes[i] > cont_sizes[largest-1]) {
+            next = largest;
+            largest = i + 1;
+        } else if (cont_sizes[i] > cont_sizes[next-1]) {
+            next = i + 1;
+        }
+    }
+
+    if (cont_sizes[next] < 400) {
+        next = 0;
+    }
+
+    place(map, WARP_SWAMP_SOUTH, TILE_CAVE, next ? next : largest, next);
+    place(map, WARP_SWAMP_NORTH, TILE_CAVE, largest, 0);
+    place(map, WARP_NORTHERN_SHRINE, TILE_CAVE, largest, next);
+    place(map, WARP_MOUNTAIN_CAVE, TILE_CAVE, largest, next);
+    place(map, WARP_SOUTHERN_SHRINE, TILE_CAVE, largest, next);
+    place(map, WARP_ERDRICKS_CAVE, TILE_CAVE, largest, next);
+    place(map, WARP_TANTEGEL_BASEMENT, TILE_CAVE, largest, next);
+    place(map, WARP_GARINS_GRAVE, TILE_CAVE, largest, next);
+
+    place(map, WARP_GARINHAM, TILE_TOWN, largest, next);
+    place(map, WARP_KOL, TILE_TOWN, largest, next);
+    place(map, WARP_BRECCONARY, TILE_TOWN, largest, next);
+    place(map, WARP_RIMULDAR, TILE_TOWN, largest, next);
+    place(map, WARP_HAUKSNESS, TILE_TOWN, largest, next);
+    place(map, WARP_CANTLIN, TILE_TOWN, largest, next);
+
+    place(map, WARP_TANTEGEL, TILE_CASTLE, largest, next);
+    place(map, WARP_CHARLOCK, TILE_CASTLE, largest, next);
 }
 
 static int map_fill_point(dw_rom *rom, uint8_t *points, 
@@ -105,16 +167,6 @@ static int map_fill_point(dw_rom *rom, uint8_t *points,
     if (tile == TILE_MOUNTAIN)
         return 2; /* not so many mountain tiles */
     return 1;
-}
-
-static int points_len(int *p) {
-    int ret = -2;
-    while (*p >= 0) {
-        printf("%d\n", *p);
-        p++;
-        ret++;
-    }
-    return ret/2;
 }
 
 static void map_fill(dw_rom *rom, dw_tile tile, int size)
@@ -222,42 +274,71 @@ static int map_continent(dw_map *map, uint8_t x, uint8_t y,
     return size;
 }
 
-static inline int find_walkable_area(dw_rom *rom, int *cont_sizes)
+static bool inline needs_bridge(uint8_t left, uint8_t right, int *cont_sizes)
+{
+    if (!left || !right || left == right)
+        return false;
+    if (cont_sizes[left-1] < 100 || cont_sizes[right-1] < 100)
+        return false;
+    return true;
+}
+
+static bool add_bridges(dw_map *map, int *cont_sizes)
+{
+    /* this could use more work */
+    uint8_t x, y, left, right;
+    bool added = false;
+
+    for (y=0; y < 120; y++) {
+        for (x=2; x < 118; x++) {
+            if (map->tiles[x][y] == TILE_WATER) {
+                left = map->walkable[x-1][y];
+                right = map->walkable[x+1][y];
+                if (needs_bridge(left, right, cont_sizes)){
+                    map->tiles[x][y] = TILE_BRIDGE;
+                    added = true;
+                }
+            }
+        }
+    }
+    return added; 
+}
+
+static inline int find_walkable_area(dw_map *map, int *cont_sizes)
 {
     int x, y, size, largest = 0;
     uint8_t continent = 0;
-    dw_map *map = &rom->map;
 
     memset(map->walkable, 0, 120 * 120);
     for (y=0; y < 120; y++) {
         for (x=0; x < 120; x++) {
             if (!map->walkable[x][y] && tile_is_walkable(map->tiles[x][y])) {
                 cont_sizes[continent] = 
-                        map_continent(&rom->map, x, y, continent+1);
+                        map_continent(map, x, y, continent+1);
                 continent++;
             }
         }
     }
-    for (y=0; y < 120; y++) {
-        for (x=0; x < 120; x++) {
-            if (rom->map.walkable[x][y])
-                printf("%x ", rom->map.walkable[x][y]);
-            else
-                printf("  ");
-        }
-        printf("\n");
-    }
-    printf("\n");
-    for (x=0; x < continent; x++) {
-        printf("%d ", cont_sizes[x]);
-    }
-    printf("\n");
+//    for (y=0; y < 120; y++) {
+//        for (x=0; x < 120; x++) {
+//            if (map->walkable[x][y])
+//                printf("%x ", map->walkable[x][y]);
+//            else
+//                printf("  ");
+//        }
+//        printf("\n");
+//    }
+//    printf("\n");
+//    for (x=0; x < continent; x++) {
+//        printf("%d ", cont_sizes[x]);
+//    }
+//    printf("\n");
     return (int)continent;
 }
 
 bool map_generate_terrain(dw_rom *rom)
 {
-    int i, j, size, continents, continent_sizes[256];
+    int i, j, size, continents, continent_sizes[256], viable_continents, *v;
 
     dw_tile tiles[16] = {
             TILE_GRASS,  TILE_GRASS,  TILE_GRASS, TILE_SWAMP,
@@ -280,7 +361,10 @@ bool map_generate_terrain(dw_rom *rom)
     map_smooth(rom);
 
     /* zero out the walkability data */
-    continents = find_walkable_area(rom, continent_sizes);
+    continents = find_walkable_area(&rom->map, continent_sizes);
+    if (add_bridges(&rom->map, continent_sizes))
+        continents = find_walkable_area(&rom->map, continent_sizes);
+    place_landmarks(&rom->map, continent_sizes, continents);
     return map_encode(rom);
 }
 

eb0f94e29b5fc6246d3f9a9c4671419ca91a8de5
Thomas McGrew <tjmcgrew@gmail.com>
2019-07-24 21:57:51 -0400

    Sprite updates for Thief and Tov

--- tools/patchgen.py
+++ tools/patchgen.py
@@ -15,7 +15,7 @@ def main():
     changed.close()
 
     for r in p.records:
-        print("vpatch(rom, 0x%05x, %4d, " %
+        print("    vpatch(rom, 0x%05x, %4d, " %
                 (r.address - 0x10, len(r.content)), end=" ")
         for b in r.content[:-1]:
             print("0x%02x, " % b, end=" ")

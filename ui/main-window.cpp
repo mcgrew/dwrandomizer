bc55926ce6abf252a7b1ee17a8b8ec0326443c1e
Thomas McGrew <tjmcgrew@gmail.com>
2019-09-04 23:25:37 -0400

    Moved "Scared Metal Slimes" to the monsters tab

--- ui/main-window.cpp
+++ ui/main-window.cpp
@@ -147,16 +147,16 @@ void MainWindow::layout()
     this->addOption('R', "Enable Menu Wrapping",               FEATURES,  0, 0);
     this->addOption('D', "Enable Death Necklace",              FEATURES,  1, 0);
     this->addOption('T', "Enable Torches In Battle",           FEATURES,  2, 0);
-    this->addOption('l', "Scared Metal Slimes",       "", "e", FEATURES,  0, 1);
-    this->addOption('b', "Big Swamp",                 "", "v", FEATURES,  1, 1);
-    this->addOption('r', "Repel in dungeons",                  FEATURES,  2, 1);
-    this->addOption('p', "Permanent repel",                    FEATURES,  3, 1);
+    this->addOption('b', "Big Swamp",                 "", "v", FEATURES,  0, 1);
+    this->addOption('r', "Repel in dungeons",                  FEATURES,  1, 1);
+    this->addOption('p', "Permanent repel",                    FEATURES,  2, 1);
 
     /* Monster flags */
-    this->addOption('P', "Random Monster Abilities",           MONSTERS,   0, 0);
-    this->addOption('Z', "Random Monster Zones",               MONSTERS,   1, 0);
-    this->addOption('e', "Random Monster Stats",      "Z", "", MONSTERS,   2, 0);
-    this->addOption('d', "Random Monster XP && Gold",          MONSTERS,   3, 0);
+    this->addOption('P', "Random Monster Abilities",           MONSTERS,  0, 0);
+    this->addOption('Z', "Random Monster Zones",               MONSTERS,  1, 0);
+    this->addOption('e', "Random Monster Stats",      "Z", "", MONSTERS,  2, 0);
+    this->addOption('d', "Random Monster XP && Gold",          MONSTERS,  3, 0);
+    this->addOption('l', "Scared Metal Slimes",       "", "e", MONSTERS,  0, 1);
 
     /* Shortcuts */
     this->addOption('t', "Fast Text",                          SHORTCUTS, 0, 0);

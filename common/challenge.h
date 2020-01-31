a89edb53b9279994023d158619d5ee6844be2963
Thomas McGrew <tjmcgrew@gmail.com>
2020-01-31 00:44:29 -0500

    Added some missing files from the last commit

---
+++ common/challenge.h
@@ -0,0 +1,18 @@
+#ifndef DWRANDOMIZER_CHALLENGE_H
+#define DWRANDOMIZER_CHALLENGE_H
+
+#include "dwr_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void invisible_hero(dw_rom *rom);
+void invisible_npcs(dw_rom *rom);
+void no_numbers(dw_rom *rom);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //DWRANDOMIZER_CHALLENGE_H

be83b6d4f000c38b6d25dcdd6bfb1c0f18b318ac
Thomas McGrew <tjmcgrew@gmail.com>
2020-08-09 11:32:08 -0400

    Added data and code used for growth curves
    Added code and data used to generate growth curves for documentation
    purposes

---
+++ tools/polyfit.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+# Sample run:
+# xargs python3 polyfit.py < curve_data/player_str.txt
+
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    y = np.array(sorted([float(y.strip()) for y in sys.argv[2].split(',')]))
+    x = np.array(sorted([float(x.strip()) for x in sys.argv[3].split(',')]))
+    x_range = range(0, int(max(x)+1))
+    try:
+        coef = np.polyfit(x, y, int(sys.argv[1]))
+    except TypeError as e:
+        print(f"y={len(y)}, x={len(x)}")
+        print(str(e))
+        sys.exit()
+    poly = np.poly1d(coef)
+    poly_values2 = [poly(v) for v in x_range]
+    plt.plot(x_range, poly_values2, label="New")
+
+
+    coef = list(reversed(coef))
+    plt.grid(True)
+
+    print('Coefficients:')
+    print('{')
+    print('    MIN,')
+    print('    MAX,')
+    for i in range(11):
+        if len(coef) > i:
+            print(f'    {coef[i]},')
+        else:
+            print(f'    0.0,')
+    print('};')
+
+
+    plt.scatter(x, y, s=10, label="Fit points")
+    label = None
+    for arg in sys.argv[4:]:
+        if arg.startswith('#'):
+            label = arg[1:]
+            continue
+        if arg.startswith('^'):
+            factors = [float(x) for x in arg[1:].split(',')]
+            poly = np.poly1d(list(reversed(factors)))
+            val = np.array([poly(y) for y in x_range])
+        else:
+            val = np.array([float(y) for y in arg.split(',')])
+        plt.plot(range(0, len(val)), val, label=label)
+        label = None
+    plt.legend()
+    plt.show()
+

b61207cb4454bcbf6009535e41c4453f2fd7392e
Thomas McGrew <tjmcgrew@gmail.com>
2021-07-25 17:31:41 -0400

    More work on the web ui - almost final

--- webui/base32.js
+++ webui/base32.js
@@ -114,7 +114,6 @@ function Decoder() {
                 char = String.fromCharCode(char)
             }
         }
-        char = char.toLowerCase()
         var val = lookup()[char]
         if (typeof val == 'undefined') {
             // character does not exist in our lookup table
@@ -173,76 +172,15 @@ function decode(input) {
     return output
 }
 
-/**
- * sha1 functions wrap the hash function from Node.js
- *
- * Several ways to use this:
- *
- *     var hash = base32.sha1('Hello World')
- *     base32.sha1(process.stdin, function (err, data) {
- *       if (err) return console.log("Something went wrong: " + err.message)
- *       console.log("Your SHA1: " + data)
- *     }
- *     base32.sha1.file('/my/file/path', console.log)
- */
-
-// var crypto, fs
-// function sha1(input, cb) {
-//     if (typeof crypto == 'undefined') crypto = require('crypto')
-//     var hash = crypto.createHash('sha1')
-//     hash.digest = (function(digest) {
-//         return function() {
-//             return encode(digest.call(this, 'binary'))
-//         }
-//     })(hash.digest)
-//     if (cb) { // streaming
-//         if (typeof input == 'string' || Buffer.isBuffer(input)) {
-//             try {
-//                 return cb(null, sha1(input))
-//             } catch (err) {
-//                 return cb(err, null)
-//             }
-//         }
-//         if (!typeof input.on == 'function') return cb({ message: "Not a stream!" })
-//         input.on('data', function(chunk) { hash.update(chunk) })
-//         input.on('end', function() { cb(null, hash.digest()) })
-//         return
-//     }
-// 
-//     // non-streaming
-//     if (input) {
-//         return hash.update(input).digest()
-//     }
-//     return hash
-// }
-// sha1.file = function(filename, cb) {
-//     if (filename == '-') {
-//         process.stdin.resume()
-//         return sha1(process.stdin, cb)
-//     }
-//     if (typeof fs == 'undefined') fs = require('fs')
-//     return fs.stat(filename, function(err, stats) {
-//         if (err) return cb(err, null)
-//         if (stats.isDirectory()) return cb({ dir: true, message: "Is a directory" })
-//         return sha1(require('fs').createReadStream(filename), cb)
-//     })
-// }
-// 
 var base32 = {
     Decoder: Decoder,
     Encoder: Encoder,
     encode: encode,
     decode: decode,
-    sha1: sha1
 }
 
 if (typeof window !== 'undefined') {
   // we're in a browser - OMG!
   window.base32 = base32
-}
-
-if (typeof module !== 'undefined' && module.exports) {
-  // nodejs/browserify
-  module.exports = base32
 }
 })();
---
+++ webui/index.js
@@ -0,0 +1,178 @@
+
+$ = document.querySelector.bind(document)
+$$ = document.querySelectorAll.bind(document)
+
+function require(jsFile) {
+    let el = document.createElement('script');
+    el.setAttribute('src', jsFile);
+    document.head.append(el);
+    return el;
+}
+
+
+require('interface.js');
+require('dwrandomizer.js');
+require('base32.js');
+
+let rom;
+let sprite_choices = [];
+let log = ''
+let error = ''
+function stdout(asciiCode) {
+    new_char = String.fromCharCode(asciiCode);
+    log += new_char
+    if (new_char.endsWith('\n')) {
+        console.log(log);
+        log = '';
+    }
+}
+
+function stderr(asciiCode) {
+    new_char = String.fromCharCode(asciiCode);
+    error += new_char
+    if (new_char.endsWith('\n')) {
+        console.error(error);
+        error = '';
+    }
+}
+
+class Rom extends Uint8Array {
+    header() {
+        return this.slice(0, 16);
+    }
+
+    set(addr, data) {
+        for (let i = 0; i < data.length; i++) {
+            // add 16 to skip the header
+            this[addr + 16 + i] = data[i];
+        }
+    }
+
+    randomize(seed, flags, sprite) {
+        console.log("Randomizing...");
+        FS.writeFile('/'+this.name, this);
+
+        this.seed = seed;
+        this.flags = flags;
+        Module.ccall('dwr_randomize', 'BigInt',
+            ['string', 'BigInt', 'string', 'string', 'string'],
+            ['/'+this.name, seed, flags, sprite, '/']);
+        this.outname = 'DWRando.' + seed + '.' + flags + '.nes';
+    }
+
+    save() {
+        this.output = FS.readFile('/'+this.outname, null);
+        let url = URL.createObjectURL(new Blob([this.output]), {
+            type: 'application/octet-stream'
+        });
+        let downloader = document.createElement('a');
+        downloader.href = url;
+        downloader.download = this.outname;
+        downloader.click()
+    }
+}
+
+function setup_ui() {
+    ui = new Interface(10);
+    ui.addTab('Gameplay');
+    ui.addTab('Features');
+    ui.addTab('Monsters');
+    ui.addTab('Shortcuts');
+    ui.addTab('Challenge');
+    ui.addTab('Goals');
+    ui.addTab('Cosmetic');
+    ui.setActiveTab('Gameplay');
+
+    ui.addTriOption('Gameplay', 0, 0, 6, 'Shuffle Chests & Searches');
+    ui.addTriOption('Gameplay', 2, 0, 4, 'Random Chest Locations');
+    ui.addTriOption('Gameplay', 4, 0, 2, 'Random Growth');
+    ui.addTriOption('Gameplay', 6, 0, 0, 'Random Map');
+    ui.addTriOption('Gameplay', 8, 1, 6, 'Random Spell Learning');
+    ui.addTriOption('Gameplay', 1, 1, 4, 'Random Weapon Shops');
+    ui.addTriOption('Gameplay', 3, 1, 2, 'Random Weapon Prices');
+    ui.addTriOption('Gameplay', 5, 1, 0, 'Random XP Requirements');
+    ui.addTriOption('Gameplay', 7, 2, 6, 'Heal/Hurt Before "More"');
+
+    ui.addTriOption('Features', 0, 3, 6, 'Enable Menu Wrapping');
+    ui.addTriOption('Features', 2, 3, 4, 'Enable Death Necklace');
+    ui.addTriOption('Features', 4, 3, 2, 'Enable Torches In Battle');
+    ui.addTriOption('Features', 1, 3, 0, 'Big Swamp');
+    ui.addTriOption('Features', 3, 4, 6, 'Repel in Dungeons');
+    ui.addTriOption('Features', 5, 4, 4, 'Permanent Repel');
+    ui.addTriOption('Features', 7, 4, 2, 'Permanent Torch');
+
+    ui.addTriOption('Monsters', 0, 5, 0, 'Random Monster Abilities');
+    ui.addTriOption('Monsters', 2, 5, 0, 'Random Monster Zones');
+    ui.addTriOption('Monsters', 4, 5, 0, 'Random Monster Stats');
+    ui.addTriOption('Monsters', 6, 5, 0, 'Random Monster XP & Gold');
+    ui.addTriOption('Monsters', 8, 6, 6, 'Make Random Stats Consistent');
+    ui.addTriOption('Monsters', 1, 6, 4, 'Scared Metal Slimes');
+    ui.addTriOption('Monsters', 3, 6, 2, 'Scaled Metal Slime XP');
+
+    ui.addTriOption('Shortcuts', 0, 7, 6, 'Fast Text');
+    ui.addTriOption('Shortcuts', 2, 7, 4, 'Speed Hacks');
+    ui.addTriOption('Shortcuts', 4, 7, 2, 'Open Charlock');
+    ui.addTriOption('Shortcuts', 6, 7, 0, 'Short Charlock');
+    ui.addTriOption('Shortcuts', 8, 8, 6, "Don't Require Magic Keys");
+    // leveling speed
+    ui.addDropDown('Shortcuts', 1, 8, 4, 'Leveling Speed: ', {
+        'Normal' : 0,
+        'Fast' : 1,
+        'Very Fast': 2
+    });
+
+    ui.addTriOption('Challenge', 0, 9, 0, 'No Hurtmore');
+    ui.addTriOption('Challenge', 2, 9, 0, 'No Numbers');
+    ui.addTriOption('Challenge', 4, 9, 0, 'Invisible Hero');
+    ui.addTriOption('Challenge', 6, 9, 0, 'Invisible NPCs');
+
+    ui.addTriOption('Goals', 0, 8, 4, 'Cursed Princess');
+    ui.addTriOption('Goals', 2, 8, 2, "Three's Company");
+
+    ui.addOption('Cosmetic', 0, 9, 7, 'Shuffle Music');
+    ui.addOption('Cosmetic', 2, 9, 6, 'Disable Music');
+    ui.addOption('Cosmetic', 4, 9, 5, 'Modern Spell Names');
+    ui.addOption('Cosmetic', 6, 9, 4, 'Noir Mode');
+    ui.addOption('Cosmetic', 8, 9, 3, 'Disable Spell Flashing');
+
+    // player sprite
+    let spriteBox = ui.addTextBox('Cosmetic', 1, 'Player Sprite: ');
+    spriteBox.setAttribute('list', 'sprites');
+    spriteBox.value = localStorage.getItem('sprite') || 'Random';
+    spriteBox.addEventListener('change', function(event) {
+        if (sprite_choices.includes(this.value)) {
+            this.classList.remove('invalid');
+            localStorage.setItem('sprite', this.value);
+        } else {
+            this.classList.add('invalid');
+        }
+    });
+}
+
+window.addEventListener('load', event => {
+    Module["noFSInit"] = true;
+    FS.init(() => {}, stdout, stdout);
+
+    setup_ui();
+    Module.onRuntimeInitialized = () => {
+        ui.setVersion(Module.ccall('version', 'string'));
+        let sprite_name = Module.cwrap('sprite_name', 'string', ['number']);
+        let sprites_datalist = $('datalist#sprites');
+        let i=0;
+        while(true) {
+            let name = sprite_name(i++);
+            if (!name) break;
+            let option = document.createElement('option');
+            option.value = option.innerText = name;
+            sprite_choices.push(name);
+            sprites_datalist.append(option);
+        }
+    };
+
+    let rom_data = localStorage.getItem('rom_data');
+    if (rom_data) {
+        rom = new Rom(rom_data.split(','));
+        ui.setInputFile(localStorage.getItem('rom_name'));
+    }
+});
+
